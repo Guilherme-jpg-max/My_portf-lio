59a603ee74fd46657f8a03f5352fec04675745b5
Guilherme-jpg-max <guicasii5@gmail.com>
2025-09-16 14:40:56 -0300

    criando o arquivo github_utils.py, arquivo responsavel por conectar a minha conta do github, selecionar alguns repositorios publicos que eu tenha selecionado, para deixar listado na parte de projetos, atualmente seram apenas três.

---
+++ app/github_utils.py
@@ -0,0 +1,22 @@
+import os
+import requests
+
+def get_github_repos():
+    github_user = "Guilherme-jpg-max"
+    token = os.getenv("GITHUB_TOKEN")
+    
+    if not token:
+        raise ValueError("Token do GitHub não encontrado!")
+    
+    headers = {"Authorization": f"token {token}"}
+    url = f"https://api.github.com/users/{github_user}/repos"
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        repos = response.json()
+        
+        selected_repos = ["#", "#", "#"]
+        filtered_repos = [repo for repo in repos if repo["name"] in selected_repos]
+        return filtered_repos
+    else:
+        return []
