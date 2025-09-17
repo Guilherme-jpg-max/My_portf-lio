e8b385e3783347a1ef8e3dc4908a27d42f07848a
Guilherme-jpg-max <guicasii5@gmail.com>
2025-09-17 01:00:05 -0300

    Adicionando função create app para iniciar o app

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_mail import Mail
+
+mail = Mail()
+
+def create_app():
+    app = Flask(__name__, static_folder="static")
+    
+    app.config.from_pyfile("../config.py")
+    
+    mail.init_app(app)
+    
+    from . import routes
+    app.register_blueprint(routes.bp)
+    
+    return app
